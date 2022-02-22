ffd4f324f88b1c231dd17c3bdf28fcd3b8ab5d63
Windmill-City <1449182174@qq.com>
2022-02-22 13:24:39 +0800

    Finish test window

--- testWnd/src/InputWindow.hpp
+++ testWnd/src/InputWindow.hpp
@@ -18,16 +18,27 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void debugShowInputProcessor(std::shared_ptr<const IngameIME::InputProcessor> proc)
+{
+    wprintf(L"[%S][%wS][%wS]: %wS\n",
+            proc->type == IngameIME::InputProcessorType::KeyboardLayout ? "KL" : "TIP",
+            proc->locale->locale.c_str(),
+            proc->locale->name.c_str(),
+            proc->name.c_str());
+}
+
 class InputWindow {
   public:
     static std::map<GLFWwindow*, InputWindow*> WindowMap;
 
   public:
-    int                                      m_Width;
-    int                                      m_Height;
-    GLFWwindow*                              m_Window;
-    std::shared_ptr<IngameIME::InputContext> m_InputContext;
-    bool                                     m_FullScreen = false;
+    int                                      width;
+    int                                      height;
+    GLFWwindow*                              window;
+    std::shared_ptr<IngameIME::InputContext> inputCtx;
+    bool                                     fullscreen = false;
+
+    std::shared_ptr<const IngameIME::InputProcessor> initialProc;
 
   public:
     static InputWindow* getByGLFWwindow(GLFWwindow* window)
@@ -38,96 +49,108 @@ class InputWindow {
   public:
     InputWindow()
     {
-        m_Window            = glfwCreateWindow(m_Width = 800, m_Height = 600, "libtf", NULL, NULL);
-        WindowMap[m_Window] = this;
+        window            = glfwCreateWindow(width = 800, height = 600, "libtf", NULL, NULL);
+        WindowMap[window] = this;
 
-        if (!m_Window) throw std::exception("Failed to create window");
+        if (!window) throw std::exception("Failed to create window");
 
-        glfwMakeContextCurrent(m_Window);
-        glfwSetKeyCallback(m_Window, key_callback);
-        glfwSetWindowSizeCallback(m_Window, window_size_callback);
-        glfwSetFramebufferSizeCallback(m_Window, framebuffer_size_callback);
+        glfwMakeContextCurrent(window);
+        glfwSetKeyCallback(window, key_callback);
+        glfwSetWindowSizeCallback(window, window_size_callback);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
         // glad: load all OpenGL function pointers
         if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) throw std::exception("Failed to initialize GLAD");
 
         // Create InputContext
-        m_InputContext = IngameIME::Global::getInstance().getInputContext(glfwGetWin32Window(m_Window));
+        inputCtx = IngameIME::Global::getInstance().getInputContext(glfwGetWin32Window(window));
 
         // Register callbacks
-        m_InputContext->comp->IngameIME::PreEditCallbackHolder::setCallback([this](auto&& state, auto&& ctx) {
+        inputCtx->comp->IngameIME::PreEditCallbackHolder::setCallback([this](auto&& state, auto&& ctx) {
             switch (state) {
                 case IngameIME::CompositionState::Begin: wprintf(L"Composition Begin!\n"); break;
                 case IngameIME::CompositionState::Update:
                     wprintf(L"Composition Update!\n");
-                    wprintf(L"PreEdit: %S\n", ctx->content.c_str());
+                    wprintf(L"PreEdit: %wS\n", ctx->content.c_str());
                     wprintf(L"Sel: %d-%d\n", ctx->selStart, ctx->selEnd);
                     break;
                 case IngameIME::CompositionState::End: wprintf(L"Composition End\n"); break;
             }
         });
 
-        m_InputContext->comp->IngameIME::CommitCallbackHolder::setCallback([this](auto&& commit) {
+        inputCtx->comp->IngameIME::CommitCallbackHolder::setCallback([this](auto&& commit) {
             // Show commit
-            wprintf(L"Commit: %S\n", commit.c_str());
+            wprintf(L"Commit: %wS\n", commit.c_str());
         });
 
-        m_InputContext->comp->IngameIME::PreEditRectCallbackHolder::setCallback([this](auto&& rect) {
+        inputCtx->comp->IngameIME::PreEditRectCallbackHolder::setCallback([this](auto&& rect) {
             rect.left   = 20;
             rect.top    = 20;
             rect.right  = 20;
             rect.bottom = 20;
         });
 
-        m_InputContext->comp->IngameIME::CandidateListCallbackHolder::setCallback([this](auto&& state, auto&& ctx) {
+        inputCtx->comp->IngameIME::CandidateListCallbackHolder::setCallback([this](auto&& state, auto&& ctx) {
             switch (state) {
                 case IngameIME::CandidateListState::Begin: wprintf(L"CandidateList Begin!\n"); break;
                 case IngameIME::CandidateListState::Update:
                     wprintf(L"CandidateList Update!\n");
                     wprintf(L"Selection: %d\n", ctx->selection);
-                    for (auto&& cand : ctx->candidates) { wprintf(L"%S\n", cand.c_str()); }
+                    for (auto&& cand : ctx->candidates) { wprintf(L"%wS\n", cand.c_str()); }
                     break;
                 case IngameIME::CandidateListState::End: wprintf(L"CandidateList End!\n"); break;
             }
         });
 
-        m_InputContext->setActivated(true);
-        m_InputContext->setFullScreen(true);
+        IngameIME::Global::getInstance().setCallback([this](auto&& state, auto&& ctx) {
+            switch (state) {
+                case IngameIME::InputProcessorState::FullUpdate: wprintf(L"InputProcessor Full Update!\n"); break;
+                case IngameIME::InputProcessorState::InputModeUpdate:
+                    wprintf(L"InputProcessor InputMode Update!\n");
+                    break;
+            }
+
+            debugShowInputProcessor(ctx.proc);
+
+            wprintf(L"InputModes:\n");
+            for (auto&& mode : ctx.modes) { wprintf(L"%wS\n", mode.c_str()); }
+        });
+
+        wprintf(L"Active InputProc:\n");
+        initialProc = IngameIME::Global::getInstance().getActiveInputProcessor();
+        debugShowInputProcessor(initialProc);
+
+        wprintf(L"InputProcs:\n");
+        for (auto proc : IngameIME::Global::getInstance().getInputProcessors()) { debugShowInputProcessor(proc); }
     }
 
   public:
     void runEventLoop()
     {
-        while (!glfwWindowShouldClose(m_Window)) {
+        while (!glfwWindowShouldClose(window)) {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
-            glfwSwapBuffers(m_Window);
+            glfwSwapBuffers(window);
             glfwPollEvents();
         }
     }
     void switchFullScreen()
     {
-        m_FullScreen = !m_FullScreen;
+        fullscreen = !fullscreen;
         // Update FullScreen state
-        m_InputContext->setFullScreen(m_FullScreen);
+        inputCtx->setFullScreen(fullscreen);
 
-        wprintf(L"FullScreen:%wS\n", m_FullScreen ? L"True" : L"False");
+        wprintf(L"FullScreen:%wS\n", fullscreen ? L"True" : L"False");
 
         auto monitor = glfwGetPrimaryMonitor();
         int  monitorWidth, monitorHeight;
         glfwGetMonitorWorkarea(monitor, NULL, NULL, &monitorWidth, &monitorHeight);
 
-        if (m_FullScreen) {
-            glfwSetWindowMonitor(m_Window, monitor, 0, 0, monitorWidth, monitorHeight, GLFW_DONT_CARE);
-        }
-        else
-            glfwSetWindowMonitor(m_Window,
-                                 NULL,
-                                 (monitorWidth - m_Width) / 2,
-                                 (monitorHeight - m_Height) / 2,
-                                 m_Width,
-                                 m_Height,
-                                 GLFW_DONT_CARE);
+        // if (fullscreen) { glfwSetWindowMonitor(window, monitor, 0, 0, monitorWidth, monitorHeight, GLFW_DONT_CARE); }
+        // else
+        //     glfwSetWindowMonitor(
+        //         window, NULL, (monitorWidth - width) / 2, (monitorHeight - height) / 2, width, height,
+        //         GLFW_DONT_CARE);
     }
 };
 
@@ -139,13 +162,22 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) { glfwSetWindowShouldClose(window, GLFW_TRUE); }
     // Switch Fullscreen
     if (glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS) { InputWindow::getByGLFWwindow(window)->switchFullScreen(); }
+    // Switch Activate
+    if (glfwGetKey(window, GLFW_KEY_F5) == GLFW_PRESS) {
+        auto inputCtx = InputWindow::getByGLFWwindow(window)->inputCtx;
+        inputCtx->setActivated(!inputCtx->getActivated());
+    }
+    // Activate Initial InputProc
+    if (glfwGetKey(window, GLFW_KEY_F3) == GLFW_PRESS) {
+        InputWindow::getByGLFWwindow(window)->initialProc->setActivated();
+    }
 }
 
 void window_size_callback(GLFWwindow* window, int width, int height)
 {
     auto inputWindow = InputWindow::getByGLFWwindow(window);
-    if (!inputWindow->m_FullScreen) {
-        inputWindow->m_Width  = width;
-        inputWindow->m_Height = height;
+    if (!inputWindow->fullscreen) {
+        inputWindow->width  = width;
+        inputWindow->height = height;
     }
 }
